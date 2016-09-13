00853d18114a7c2901866b3822458a28ffcef7b4
adityadharne <adityadharne@gmail.com>
2016-09-12 22:30:35 -0400

    Mars habitat console simulator
    This is project from the Software Engineering course at The George
    Washington University. The aim of the project is developing a software
    application using methodologies learned.

---
+++ src/sep3/SEP3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sep3;
+
+/**
+ *
+ * @author adityadharne
+ */
+import java.io.*;
+
+public class SEP3 {
+    public static void main(String [] args) {
+        
+        login login = new login();
+        login.setVisible(true);
+        
+        //gui1 gui = new gui1();
+        //gui.setVisible(true);
+        
+
+    }
+}
+
